0cc16cbf5ba5fc41012fccbfe0e591fafa62e34f
manozita <manoelamartedi@hotmail.com>
2022-08-27 14:20:54 -0300

    tarefa 2 - commit 5

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,6 @@
-nome = 'Manoela'
-email = 'manoelamartedi@hotmail.com'
-profissao = 'Estudante'
-idade = 17
-sonho = 'California'
+var nome = 'Manoela';
+var email = 'manoelamartedi@hotmail.com';
+var profissao = 'Estudante';
+var idade = 17;
+var sonho = 'California';
+console.log('Olá,',nome + '!','Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para',sonho + '. Confirma pra gente alguns dados? A sua idade é', idade +', você é',profissao,'e o seu email é', email +'? Ficamos no aguardo. Muito obrigado e boa viagem! :D')
